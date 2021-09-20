d8ce8ff5a9ed821b2563decd3356007e1c757c75
SandrineTiss <87864253+SandrineTiss@users.noreply.github.com>
2021-09-20 17:30:48 +0200

    Commit

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -22,6 +22,8 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         parent::__construct($registry, User::class);
     }
 
+
+
     /**
      * Used to upgrade (rehash) the user's password automatically over time.
      */
