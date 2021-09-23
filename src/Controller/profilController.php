95729e15673b161adbb2a64154fecde35b0b8c79
SandrineTiss <87864253+SandrineTiss@users.noreply.github.com>
2021-09-23 17:45:18 +0200

    affichage profil participant à une sortie en cours

--- src/Controller/profilController.php
+++ src/Controller/profilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,17 @@ class profilController extends AbstractController
     public function profil(): Response
     {
         return $this->render('profil/profilUser.html.twig');
+    }
+
+
+    /**
+     * @Route("/profil/{id}", name="profil_participant")
+     */
+    public function profilParticipant(int $id, UserRepository $userRepository) : Response
+    {
+        $participant = $userRepository->find($id);
+        return $this->render('participation/profil.html.twig',[
+            'participant' => $participant,
+        ]);
     }
 }
