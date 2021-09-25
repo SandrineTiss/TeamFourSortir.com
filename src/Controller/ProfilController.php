cf77d3a74ab000ed6816056997ca33393e432b1a
SandrineTiss <87864253+SandrineTiss@users.noreply.github.com>
2021-09-25 08:11:45 +0200

    fichier yaml créé !
    Attention, faut faire un composer --install !!!
    
    et ça fonctionne

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class profilController extends AbstractController
+class ProfilController extends AbstractController
 {
     /**
      * @Route("/profil", name="profil_user")
