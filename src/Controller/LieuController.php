5b5212f15660eebeb6a035b9df43808dff221e99
SandrineTiss <87864253+SandrineTiss@users.noreply.github.com>
2021-09-27 15:47:51 +0200

    afficher/créer/supprimer lieu OK

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -3,18 +3,80 @@
 namespace App\Controller;
 
 use App\Entity\Lieu;
+use App\Form\LieuType;
+use App\Repository\LieuRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LieuController extends AbstractController
 {
     /**
-     * @Route("/lieu", name="lieu")
+     * @Route("/lieu/creation", name="lieu_creation")
      */
-    public function addLieu(EntityManagerInterface $entityManager): Response
+    public function addLieu(EntityManagerInterface $entityManager,Request $request): Response
     {
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class, $lieu);
+        $lieuForm->handleRequest($request);
 
+        if($lieuForm->isSubmitted() && $lieuForm->isValid()){
+
+            $entityManager->persist($lieu);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le lieu de sortie a bien été créé !');
+
+            return $this->redirectToRoute('lieu_affichage');
+        }
+
+        return $this->render('lieu/creerLieu.html.twig', [
+            'lieuForm' => $lieuForm->createView(),
+        ]);
     }
+
+    /**
+     * @Route("/lieu", name="lieu_affichage")
+     */
+    public function afficheCampus( LieuRepository $lieuRepository  ): Response
+    {
+        $listeLieux = $lieuRepository->findAll();
+        return $this->render('lieu/lieux.html.twig', [
+            'listeLieux' => $listeLieux,
+        ]);
+    }
+
+    /**
+     * @Route("/lieu/{id}", name="lieu_suppression")
+     */
+    public function deleteCampus(EntityManagerInterface $entityManager,
+                                 Request $request,
+                                 LieuRepository $lieuRepository,
+                                 int $id
+    ): Response
+    {
+        $lieu = $lieuRepository->findOneBy(['id' => $id]);
+        $lieuForm = $this->createForm(LieuType::class, $lieu);
+        $lieuForm->handleRequest($request);
+
+        if($lieuForm->isSubmitted() && $lieuForm->isValid()){
+
+            $entityManager->remove($lieu);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le lieu de sortie a bien été supprimé !');
+
+            return $this->redirectToRoute('lieu_affichage');
+        }
+
+        return $this->render('lieu/supprimerLieu.html.twig', [
+            'lieuForm' => $lieuForm->createView(),
+        ]);
+    }
+
+
+
+
 }
---
+++ src/Form/LieuType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('rue')
+            ->add('latitude')
+            ->add('longitude')
+            ->add('ville', EntityType::class, [
+        'label' => 'Ville :',
+        'class' => Ville::class,
+        'choice_label' => 'nom',
+        'multiple' => false
+    ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
