aac4c5a330f98fd09057959dfd20767e07bb3b63
Shiro <guillaume.carlier@hotmail.com>
2021-09-29 23:29:16 +0200

    Commentaire du JS pour affichage dynamique de la liste déroulante

--- public/js/createSortie.js
+++ public/js/createSortie.js
@@ -1,25 +1,31 @@
-        var httpRequest = new XMLHttpRequest()
-        console.log('YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEES')
-        let ville = document.querySelector("#sortie_ville")
-        console.log(ville)
-        let lieu = document.querySelector('#sortie_lieu')
+       // Affichage d'une liste déroulante dynamique
 
+        var httpRequest = new XMLHttpRequest()
+
+       // écoute la sélection de la ville
+        let ville = document.querySelector("#sortie_ville")
         ville.addEventListener("change", function (e) {
             e.preventDefault()
             let form = document.querySelector("form")
             let data = this.name + "=" + this.value
 
-
+            // Communication avec le serveur
             httpRequest.open('POST', 'http://localhost/TeamFourSortirCom/public/sortie/creer', false)
             httpRequest.setRequestHeader('X-Requested-With', 'xmlhttprequest')
+
+            // Envoie du formulaire
             var test = new FormData(form)
             httpRequest.send(test);
+
+            // Récupération de la réponse serveur avec les lieux correspondant aux villes
             var resp = httpRequest.response
 
+            // Conversion de la réponse
             let content = document.createElement("html");
             content.innerHTML = resp;
+
+            // injection de la réponse dans l'élément souhaité
             let nouveauSelect = content.querySelector("#sortie_lieu");
-            console.log(nouveauSelect);
             document.querySelector("#sortie_lieu").replaceWith(nouveauSelect);
 
         })
